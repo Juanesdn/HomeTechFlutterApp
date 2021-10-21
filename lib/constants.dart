832f545ce7c54c4085306b3e2fa6fc54d95784b3
florozco <florozco@uninorte.edu.co>
2021-10-21 09:14:35 -0500

    basic implementation

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,8 +4,18 @@ const primaryColor = Color(0xFFFF7643);
 const primaryGradientColor = LinearGradient(
   begin: Alignment.topLeft,
   end: Alignment.bottomRight,
-  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+  colors: [Color(0xFFFF7643), Color(0xFFFFA53E)],
 );
+
+const secundaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color.fromARGB(255, 156, 44, 243),
+    Color.fromARGB(255, 251, 122, 45),
+  ],
+);
+
 const secondaryColor = Color(0xFF979797);
 const textColor = Color(0xFF757575);
 
