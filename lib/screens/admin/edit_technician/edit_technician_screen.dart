05b527dfa4af64da5578f07767c63008a1d9de51
florozco <florozco@uninorte.edu.co>
2021-11-02 19:39:23 -0500

    Update edit_technician_screen.dart

--- lib/screens/admin/edit_technician/edit_technician_screen.dart
+++ lib/screens/admin/edit_technician/edit_technician_screen.dart
@@ -4,24 +4,44 @@ import 'package:hometech_app/constants.dart';
 
 class EditTechnicianScreen extends StatefulWidget {
   const EditTechnicianScreen({Key? key}) : super(key: key);
-
   @override
   _EditTechnicianScreenState createState() => _EditTechnicianScreenState();
+}
+
+class TechnicianListDataModel {
+  String technician_name;
+  String technician_specialization;
+  TechnicianListDataModel(this.technician_name, this.technician_specialization);
 }
 
 class _EditTechnicianScreenState extends State<EditTechnicianScreen> {
   @override
   void initState() {
     super.initState();
-    setState(() {});
+    setState(() {
+      _technicianChoose = technicianDataList[0];
+    });
   }
 
-  defaultTextFormField(String label) => TextFormField(
-        maxLines: 1,
-        style: TextStyle(
-          foreground: null,
-          color: Color(0xFF2B2B2B),
-        ),
+  //String _technicianChoose;
+  TechnicianListDataModel? _technicianChoose;
+
+  void _onDropDownItemSelected(TechnicianListDataModel? newSelectedTechnician) {
+    setState(() {
+      _technicianChoose = newSelectedTechnician;
+    });
+  }
+
+  List<TechnicianListDataModel> technicianDataList = [
+    //TechnicianListDataModel("Si", "Seleccione un tipo de tecnico"),
+    TechnicianListDataModel("Luis ", "Bocinas"),
+    TechnicianListDataModel("Juan", "Teclados"),
+    TechnicianListDataModel("Valeria", "Laptops"),
+  ];
+
+  defaultTextFormField(String label, int i) => TextFormField(
+        maxLines: i,
+        style: defaultTextStyle(),
         decoration: defaultInputDecoration(label),
       );
 
@@ -42,37 +62,118 @@ class _EditTechnicianScreenState extends State<EditTechnicianScreen> {
         labelText: label,
       );
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          alignment: Alignment.topCenter,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: Color(0xFFF6F6F6),
+  defaultFormField() => FormField<String>(
+        builder: (FormFieldState<String> state) {
+          return defualtInputDecorator();
+        },
+      );
+
+  defaultTextStyle() => TextStyle(
+        color: Color(0xFF2B2B2B),
+        foreground: null,
+      );
+
+  defualtInputDecorator() => InputDecorator(
+        decoration: defaultInputDecoration(""),
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton<TechnicianListDataModel>(
+            style: defaultTextStyle(),
+            hint: Text(
+              "Especializacion",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                defaultTextFormField("Fullname"),
-                defaultTextFormField("Email"),
-                defaultTextFormField("Celular"),
-                Row(
-                  children: [
-                    Expanded(
-                      child: defaultTextFormField("Edad"),
-                    ),
-                    Expanded(
-                      child: defaultTextFormField("Experiencia"),
-                    ),
-                  ],
-                ),
-              ],
+            items: technicianDataList
+                .map<DropdownMenuItem<TechnicianListDataModel>>(
+                    (TechnicianListDataModel value) {
+              return defaultDropdownMenuItem(value);
+            }).toList(),
+            isExpanded: true,
+            isDense: true,
+            onChanged: (TechnicianListDataModel? newSelectedTechnician) {
+              _onDropDownItemSelected(newSelectedTechnician);
+            },
+            value: _technicianChoose,
+          ),
+        ),
+      );
+
+  defaultDropdownMenuItem(TechnicianListDataModel tldm) => DropdownMenuItem(
+        value: tldm,
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 15,
+            ),
+            Text(tldm.technician_specialization),
+          ],
+        ),
+      );
+
+  defaultSafeArea() => SafeArea(
+        child: Scaffold(
+          body: Container(
+            alignment: Alignment.topCenter,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Color(0xFFF6F6F6),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Bienvenido de vuelta",
+                        style: TextStyle(
+                          color: Color(0xFF2B2B2B),
+                          backgroundColor: Colors.transparent,
+                          fontSize: 34,
+                          letterSpacing: -0.41,
+                        ),
+                      ),
+                    ],
+                  ),
+                  defaultTextFormField("Fullname", 1),
+                  defaultTextFormField("Email", 1),
+                  defaultTextFormField("Celular", 1),
+                  defaultFormField(),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: defaultTextFormField("Edad", 1),
+                      ),
+                      Expanded(
+                        child: defaultTextFormField("Experiencia", 1),
+                      ),
+                    ],
+                  ),
+                  defaultTextFormField("Localizacion", 1),
+                  defaultTextFormField("Acerca de", 4),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: defaultTextFormField("Puntaje", 1),
+                      ),
+                      Expanded(
+                        child: defaultTextFormField("Precio por hora", 1),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: defaultSafeArea(),
     );
   }
 }
